250295742165667465612087e7c505dd74fc35ef
李光浩 <liguanghao@viatomtech.com>
2021-03-16 14:47:06 +0800

    Initial commit

---
+++ app/src/main/java/com/viatom/myapplication/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.viatom.myapplication
+
+import android.app.Activity
+import android.content.pm.ActivityInfo
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.FrameLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        window.setFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED)
+
+        val web=findViewById<WebView>(R.id.aa)
+        web.settings.setJavaScriptEnabled(true);//启用js
+        web.settings.blockNetworkImage = false;//解决图片不显示
+        web.settings.useWideViewPort=true
+        web.settings.loadWithOverviewMode=true
+        web.webViewClient=object: WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                return super.shouldOverrideUrlLoading(view, request)
+            }
+        }
+        web.webChromeClient=mWebChromeClient
+        web.loadUrl("https://www.youtube.com")
+    }
+    var fullscreenContainer: FrameLayout? = null
+    var customViewCallback: WebChromeClient.CustomViewCallback? = null
+    private val mWebChromeClient = object : WebChromeClient() {
+        override fun onShowCustomView(view: View?, callback: CustomViewCallback?) {
+            super.onShowCustomView(view, callback)
+            showCustomView(view, callback)
+        }
+
+        override fun onHideCustomView() {
+            super.onHideCustomView()
+            hideCustomView()
+        }
+    }
+
+    /**
+     * 显示自定义控件
+     */
+    private fun showCustomView(view: View?, callback: WebChromeClient.CustomViewCallback?) {
+        if (fullscreenContainer != null) {
+            callback?.onCustomViewHidden()
+            return
+        }
+
+        fullscreenContainer = FrameLayout(this).apply { setBackgroundColor(Color.BLACK) }
+        customViewCallback = callback
+        fullscreenContainer?.addView(view)
+        val decorView = window?.decorView as? FrameLayout
+       requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        decorView?.addView(fullscreenContainer)
+    }
+
+    /**
+     * 隐藏自定义控件
+     */
+    private fun hideCustomView() {
+        if (fullscreenContainer == null) {
+            return
+        }
+
+        val decorView =window?.decorView as? FrameLayout
+       requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        fullscreenContainer?.removeAllViews()
+        decorView?.removeView(fullscreenContainer)
+        fullscreenContainer = null
+        customViewCallback?.onCustomViewHidden()
+        customViewCallback = null
+    }
+
+}
