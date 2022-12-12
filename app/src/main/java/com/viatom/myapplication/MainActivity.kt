771e9451c286ea360cfce82471b54b2c96749f1e
liguanghao <liguanghao@viatomtech.com>
2022-12-12 11:10:36 +0800

    fghsdfv

--- app/src/main/java/com/viatom/myapplication/MainActivity.kt
+++ app/src/main/java/com/viatom/myapplication/MainActivity.kt
@@ -11,97 +11,21 @@ import android.view.WindowManager
 import android.webkit.*
 import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.appcompat.widget.AppCompatButton
+import com.viatom.myapplication.R
 
 class MainActivity : AppCompatActivity() {
-    lateinit var web:WebView
+    lateinit var jumpButton:AppCompatButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        window.setFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
-                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED)
 
-       web=findViewById<WebView>(R.id.aa)
-        web.settings.setJavaScriptEnabled(true);//启用js
-        web.settings.blockNetworkImage = false;//解决图片不显示
-        web.settings.useWideViewPort=true
-        web.settings.loadWithOverviewMode=true
-
-        web.settings.allowFileAccess=true
-        web.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-        web.getSettings().domStorageEnabled=true
-        web.settings.setAppCacheEnabled(true)
-
-        web.webViewClient=object: WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
-                super.shouldOverrideUrlLoading(view, request)
-                val url=request?.url.toString()
-                return !(url.startsWith("http://") || url.startsWith("https://"))
-
-            }
-        }
-        web.webChromeClient=mWebChromeClient
-
-        web.addJavascriptInterface(object : Any() {
-            //定义的方法
-            @JavascriptInterface
-            fun goBack() {
-                Log.e("gaga","yes")
-            }
-        }, "Android")
-        web.loadUrl("http://193.169.0.227:8080")
-    }
-    var fullscreenContainer: FrameLayout? = null
-    var customViewCallback: WebChromeClient.CustomViewCallback? = null
-    private val mWebChromeClient = object : WebChromeClient() {
-        override fun onShowCustomView(view: View?, callback: CustomViewCallback?) {
-            super.onShowCustomView(view, callback)
-            showCustomView(view, callback)
+        jumpButton=findViewById(R.id.jump_web)
+        jumpButton.setOnClickListener {
+            startActivity(Intent(this,WebActivity::class.java))
         }
 
-        override fun onHideCustomView() {
-            super.onHideCustomView()
-            hideCustomView()
-        }
-    }
-
-    /**
-     * 显示自定义控件
-     */
-    private fun showCustomView(view: View?, callback: WebChromeClient.CustomViewCallback?) {
-        if (fullscreenContainer != null) {
-            callback?.onCustomViewHidden()
-            return
-        }
-
-        fullscreenContainer = FrameLayout(this).apply { setBackgroundColor(Color.BLACK) }
-        customViewCallback = callback
-        fullscreenContainer?.addView(view)
-        val decorView = window?.decorView as? FrameLayout
-       requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-        decorView?.addView(fullscreenContainer)
-    }
-
-    /**
-     * 隐藏自定义控件
-     */
-    private fun hideCustomView() {
-        if (fullscreenContainer == null) {
-            return
-        }
-
-        val decorView =window?.decorView as? FrameLayout
-       requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-        fullscreenContainer?.removeAllViews()
-        decorView?.removeView(fullscreenContainer)
-        fullscreenContainer = null
-        customViewCallback?.onCustomViewHidden()
-        customViewCallback = null
-    }
-
-    override fun onBackPressed() {
-        web.goBack()
-        Log.e("littlePu", "back")
     }
 
 
